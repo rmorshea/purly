bbfea5ad9f7b831b871fed10d8cf0b70233f21c9
Ryan Morshead <ryan.morshead@gmail.com>
2018-06-08 20:49:15 -0700

    add package data

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
-from __future__ import print_function
+import os
+import sys
+from glob import glob
 from setuptools import find_packages
+from distutils.core import setup
 
 # the name of the project
 name = "purly"
 
-#-----------------------------------------------------------------------------
-# Minimal Python version sanity check
-#-----------------------------------------------------------------------------
+# basic paths used to gather files
+here = os.path.abspath(os.path.dirname(__file__))
+root = os.path.join(here, name)
 
-import sys
+#-----------------------------------------------------------------------------
+# Python Version Check
+#-----------------------------------------------------------------------------
 
 if sys.version_info < (3,6):
     error = "ERROR: %s requires Python version 3.6 or above." % name
@@ -16,17 +21,8 @@ if sys.version_info < (3,6):
     sys.exit(1)
 
 #-----------------------------------------------------------------------------
-# get on with it
+# requirements
 #-----------------------------------------------------------------------------
-
-import os
-from glob import glob
-
-from distutils.core import setup
-
-here = os.path.abspath(os.path.dirname(__file__))
-root = os.path.join(here, name)
-
 
 requirements = [
     'sanic',
@@ -38,22 +34,47 @@ requirements = [
 if sys.version_info < (3, 7):
     requirements.append('async_generator')
 
+#-----------------------------------------------------------------------------
+# Library Version
+#-----------------------------------------------------------------------------
 
 with open(os.path.join(root, '_version.py')) as f:
-    namespace = {}
-    exec(f.read(), {}, namespace)
-    version = namespace["__version__"]
+    _ = {}
+    exec(f.read(), {}, _)
+    version = _["__version__"]
 
+#-----------------------------------------------------------------------------
+# Library Description
+#-----------------------------------------------------------------------------
 
 with open(os.path.join(here, 'README.md')) as f:
     long_description = f.read()
 
+#-----------------------------------------------------------------------------
+# Static Files
+#-----------------------------------------------------------------------------
+
+def package_files(*path_to_files):
+    directory = os.path.join(*path_to_files)
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+
+static_files = package_files(root, 'static')
+
+#-----------------------------------------------------------------------------
+# Install It
+#-----------------------------------------------------------------------------
 
 if __name__ == '__main__':
     setup(
         name=name,
         version=version,
         packages=find_packages(),
+        package_data={'': static_files},
         description="Control the web with Python",
         long_description=long_description,
         long_description_content_type='text/markdown',
