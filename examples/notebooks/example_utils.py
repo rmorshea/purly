a9a2502c2a435fcd74b9cdeac89144cf520b916e
Ryan Morshead <ryan.morshead@gmail.com>
2018-06-13 16:34:39 -0700

    fix example utils

--- examples/notebooks/example_utils.py
+++ examples/notebooks/example_utils.py
@@ -8,8 +8,7 @@ def localhost(protocol, port=8000):
     "127.0.0.1". Instead we use ``nbserverproxy`` whose proxy is used instead.
     """
     if 'JUPYTERHUB_OAUTH_CALLBACK_URL' in os.environ:
-        if not protocol.endswith('s'):
-            protocol += 's'
+        protocol += 's'
         form = protocol + '://hub.mybinder.org%s/proxy/%s'
         auth = os.environ['JUPYTERHUB_OAUTH_CALLBACK_URL'].rsplit('/', 1)[0]
         return form % (auth, port)
