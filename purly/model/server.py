3ef780b3676bfc142082e8b988b42cfed46d7545
rmorshea <ryan.morshead@gmail.com>
2018-07-23 19:54:19 -0700

    server now drivers refresh rate

--- purly/model/server.py
+++ purly/model/server.py
@@ -1,4 +1,5 @@
 import json
+import time
 import types
 import asyncio
 from uuid import uuid4
@@ -46,7 +47,7 @@ class Server:
     def __init_subclass__(cls):
         cls._rules = cls._rules.copy()
 
-    def __init__(self):
+    def __init__(self, rate=0.1):
         self.server = Sanic()
         for name in self._rules:
             register = getattr(self, name)
@@ -54,6 +55,7 @@ class Server:
         self._connections = {}
         self._updates = {}
         self._models = {}
+        self._rate = rate
 
     def run(self, *args, **kwargs):
         self.server.run(*args, **kwargs)
@@ -76,21 +78,18 @@ class Server:
         self._updates[conn] = [initialize]
         # keep a reference to the model alive.
         try:
-            empty = 0
             while True:
+                start = time.time()
                 send = self._sync(conn)
                 await socket.send(json.dumps(send))
                 recv = json.loads(await socket.recv())
                 if recv:
                     self._load(conn, model, recv)
                 # throttle connections with few udpates
-                if not send and not recv:
-                    if empty > 2000:
-                        await asyncio.sleep(0.5)
-                    else:
-                        empty += 1
-                else:
-                    empty = 0
+                stop = time.time()
+                elapsed = stop - start
+                if elapsed < self._rate:
+                    await asyncio.sleep(self._rate - elapsed)
         except ConnectionClosed:
             pass
         except Exception:
