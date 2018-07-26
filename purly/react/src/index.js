1cf2191ffd526b33aaca9b451ad89c4ccafb92a5
rmorshea <ryan.morshead@gmail.com>
2018-07-25 23:46:42 -0700

    check socket protocol

--- purly/react/src/index.js
+++ purly/react/src/index.js
@@ -7,9 +7,14 @@ import global from './global'
 
 const uri = document.location.hostname + ":" + document.location.port;
 var url = (uri + document.location.pathname).split("/").slice(0, -1);
-url[url.length - 1] = "stream"
-console.log(url)
-const socket = new WebSocket("ws://" + url.join('/'));
+url[url.length - 1] = "stream";
+var secure = (document.location.protocol == "https");
+if (secure) {
+  var protocol = "wss://";
+} else {
+  var protocol = "ws://";
+}
+const socket = new WebSocket(protocol + url.join('/'));
 const mount = document.getElementById('react-mount');
 
 socket.onmessage = function onMessage(event) {
