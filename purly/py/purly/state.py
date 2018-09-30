550dea0299930e8a4432828091d4cbbc2ff396c0
rmorshea <ryan.morshead@gmail.com>
2018-09-30 12:21:30 -0700

    serve purly-board and update binder build

--- purly/py/purly/state.py
+++ purly/py/purly/state.py
@@ -4,14 +4,14 @@ from sanic import response
 from .utils import JS_PATH
 from .model import Server, rule
 
-WIDGET_PATH = os.path.join(JS_PATH, "packages", "purly-widget")
+BOARD_PATH = os.path.join(JS_PATH, "packages", "purly-board")
 
 
 class Machine(Server):
 
     @rule('route', '/model/<model>/assets/<path:path>')
     async def _index(self, request, model, path):
-        absolute = os.path.join(WIDGET_PATH, 'build', *path.split('\n'))
+        absolute = os.path.join(BOARD_PATH, 'build', *path.split('\n'))
         return await response.file(absolute)
 
     @rule('route', '/model/<model>/state')
